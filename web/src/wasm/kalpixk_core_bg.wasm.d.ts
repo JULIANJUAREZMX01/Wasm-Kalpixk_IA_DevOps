7f468369c61c1ef4ab4b95f1fc29d5b19e116157
Julian Juarez // jaja.dev <146510758+JULIANJUAREZMX01@users.noreply.github.com>
2026-04-08 22:03:10 -0500

    feat(wasm): actualizar bindings TypeScript/JS del core

--- web/src/wasm/kalpixk_core_bg.wasm.d.ts
+++ web/src/wasm/kalpixk_core_bg.wasm.d.ts
@@ -1,6 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
+export const analyze_and_retaliate: (a: number, b: number) => [number, number];
 export const analyze_defense_nodes: (a: number, b: number) => [number, number];
 export const check_lockdown: (a: number, b: number) => number;
 export const check_memory_bounds_wasp: (a: number, b: number, c: number) => [number, number];
@@ -12,6 +13,7 @@ export const parse_log_line: (a: number, b: number, c: number, d: number) => [nu
 export const process_batch: (a: number, b: number, c: number, d: number) => [number, number];
 export const validate_input_wasp: (a: number, b: number, c: number) => [number, number];
 export const version: () => [number, number];
+export const wasm_lockdown: (a: number, b: number, c: number, d: number, e: number) => [number, number];
 export const __wbindgen_externrefs: WebAssembly.Table;
 export const __wbindgen_malloc: (a: number, b: number) => number;
 export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
